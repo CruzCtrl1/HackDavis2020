6788a2b27e9b475ba22ec4369084fe9669471032
unknown <andrew123zhang@gmail.com>
2020-01-19 12:51:55 -0800

    asdf

--- firebase/public/MockData.js
+++ firebase/public/MockData.js
@@ -1,10 +1,12 @@
-let data = {
-    name: "request3",
-    category: "clothing",
-    completed: false,
-    location: (0,0),
-    message: "example request need clothing",
-    timestamp: ("3PM 1/19 (this should be geopoint instead of string but I ran out of time")
-}
+function addData(){
+    let data = {
+        name: "request3",
+        category: "clothing",
+        completed: false,
+        location: (0,0),
+        message: "example request need clothing",
+        timestamp: ("3PM 1/19 (this should be geopoint instead of string but I ran out of time")
+    }
+    let setDoc = db.collection("Requests").doc("misc").set(data);
 
-let setDoc = db.collection("Requests").doc("misc").set(data);
+}
